278d4216754054efbba7a726142fc89a97e2039d
sfrick <sebastian.frick@fincon.eu>
2021-07-26 09:03:01 +0200

    * add recorded motion data for testing

--- src/main/js/index.js
+++ src/main/js/index.js
@@ -1,15 +1,25 @@
 const WebSocket = require('ws');
+const fs = require('fs');
 
 const ws = new WebSocket('ws://localhost:8080/');
 
+
+let file = undefined;
 ws.on('open', function open() {
-    ws.send('SESSION');
+    ws.send('MOTION');
+    file = fs.createWriteStream('motion-data-txt');
 });
 
 ws.on('message', function incoming(msg) {
-    console.log(msg)
+    // console.log(msg);
+    file.write('\n');
+    file.write(msg);
 })
 
 ws.on('error', function (err) {
     console.error(err)
+})
+
+ws.on('close', function () {
+    file.end()
 })
